0c6ae61c69f288c21865db7ba05c7d6ddd993052
Cody <tengyuchen07@gmail.com>
2026-04-08 14:47:51 +0800

    新增管道

--- prog1.cpp
+++ prog1.cpp
@@ -1,7 +1,29 @@
 ﻿#include <iostream>
+#include <unistd.h>
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int p2c[2], c2p[2];
+    if(pipe(p2c) == -1 || pipe(c2p) == -1)
+    {
+        perror("pipe");
+        return 1;
+    }
 
+    pid_t pid = fork();
+    if(pid==-1){
+        perror("fork");
+        return 1;
+    }
+    switch(pid){
+        case 0: //child
+            close(p2c[1]);
+            close(c2p[0]);
+            while(true){
+
+            }
+        default:
+            close(p2c[0]);
+            close(c2p[1]);
+    }
 }
